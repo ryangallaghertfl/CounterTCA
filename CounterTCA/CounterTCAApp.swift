62c4cdb4f939b16f199bad5c3c376589cfc2054d
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-01 14:53:46 +0000

    main app: provided initial state

--- CounterTCA/CounterTCAApp.swift
+++ CounterTCA/CounterTCAApp.swift
@@ -5,13 +5,20 @@
 //  Created by Ryan Gallagher on 01/12/2023.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 @main
-struct CounterTCAApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+struct CounterApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView(
+        store: Store(
+          initialState: CounterFeature.State()
+        ) {
+          CounterFeature()
         }
+      )
     }
+  }
 }
