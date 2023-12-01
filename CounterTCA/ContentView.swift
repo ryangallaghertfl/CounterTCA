9f62aa4993ea5b1347eacd5bca3eee2d04334b77
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-01 13:03:55 +0000

    setup : added outline of ui

--- CounterTCA/ContentView.swift
+++ CounterTCA/ContentView.swift
@@ -8,15 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+  var body: some View {
+    Form {
+      Section {
+        Text("0")
+        Button("Decrement") {
+          //Do something
         }
-        .padding()
+        Button("Increment") {
+          //Do something
+        }
+      }
     }
+  }
 }
 
 #Preview {
