75043a465cdcac1b1e2fa3aedca8973a00e2c6b7
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-01 12:55:12 +0000

    Initial Commit

---
+++ CounterTCA/CounterTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterTCAApp.swift
+//  CounterTCA
+//
+//  Created by Ryan Gallagher on 01/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct CounterTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
