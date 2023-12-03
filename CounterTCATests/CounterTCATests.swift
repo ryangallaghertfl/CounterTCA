d3f859656c514cf78f4b9aca1dfe99d60701408e
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-03 15:38:17 +0000

    test: test timer, outline of test but need to account for side effects

--- CounterTCATests/CounterTCATests.swift
+++ CounterTCATests/CounterTCATests.swift
@@ -11,16 +11,27 @@ import XCTest
 
 @MainActor
 final class CounterTCATests: XCTestCase {
-
+    
     func testCounter() async {
         let store = TestStore(initialState: CounterFeature.State()) {
-          CounterFeature()
+            CounterFeature()
+        }
+        //as we are using value types we can compare state changes before and after state is copied
+        await store.send(.incrementButtonTapped) {
+            $0.count = 1 //we are asserting here
         }
         
-        await store.send(.incrementButtonTapped) {
-          $0.count = 1 //we are asserting here
+    }
+    
+    func testTimer() async throws {
+        let store = TestStore(initialState: CounterFeature.State()) {
+            CounterFeature()
         }
-
-      }
-
+        
+        await store.send(.toggleTimerButtonTapped) {
+          $0.isTimerOn = true
+        }
+        
+    }
+    
 }
